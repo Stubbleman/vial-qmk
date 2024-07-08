92e3e02805164dbb461bf48b1a6ae895d7159df4
Stubbleman <q19950129@gmail.com>
2024-07-08 23:06:01 +0800

    [Added] Add Dactyle Manuform Lowprofile keyboard.

---
+++ keyboards/handwired/dactyle_manuform_lowprofile/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xB2, 0x41, 0x9E, 0x72, 0x40, 0x83, 0xF7, 0x08}
+
+// ESC and Enter
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 9 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 5 }
